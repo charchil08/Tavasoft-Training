a90495580dece81f1e27ef137635a82a03158c81
charchil08 <er.charchilkajaliya@gmail.com>
2023-02-10 15:15:54 +0530

    Login page created in ndex.cshtml

---
+++ Community Investment Dotnet/CommunityInvestment/CommunityInvestment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CommunityInvestment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
