f8448b3f734b54bb58b6c45cf8ff0db743197743
charchil08 <er.charchilkajaliya@gmail.com>
2023-02-14 12:20:54 +0530

    PreacticeMvc/Assignment3 - Database first approach done

---
+++ PracticeMvc/Assignment3/Assignment3/Models/Department.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assignment3.Models
+{
+    public partial class Department
+    {
+        public Department()
+        {
+            Employees = new HashSet<Employee>();
+        }
+
+        public int DepId { get; set; }
+        public string? DepName { get; set; }
+        public string? DepLocation { get; set; }
+
+        public virtual ICollection<Employee> Employees { get; set; }
+    }
+}
