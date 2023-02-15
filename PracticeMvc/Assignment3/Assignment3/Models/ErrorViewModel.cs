4675d4696df8bf54452dbc98f33c1611c6e26c22
charchil08 <er.charchilkajaliya@gmail.com>
2023-02-15 13:56:04 +0530

    removed unnecessary dir

--- PracticeMvc/Assignment3/Assignment3/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Assignment3.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
