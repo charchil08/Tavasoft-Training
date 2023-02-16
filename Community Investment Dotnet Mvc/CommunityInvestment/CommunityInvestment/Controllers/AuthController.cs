d972480a98851a49953e619c9044dd6ed7aca6b4
charchil08 <er.charchilkajaliya@gmail.com>
2023-02-16 10:20:56 +0530

    Routing change to Auth for auth pages

---
+++ Community Investment Dotnet Mvc/CommunityInvestment/CommunityInvestment/Controllers/AuthController.cs
@@ -0,0 +1,42 @@
+﻿using CommunityInvestment.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CommunityInvestment.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly ILogger<AuthController> _logger;
+
+        public AuthController(ILogger<AuthController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult LostPassword()
+        {
+            return View();
+        }
+
+        public IActionResult ResetPassword()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
--- Community Investment Dotnet Mvc/CommunityInvestment/CommunityInvestment/Controllers/HomeController.cs
+++ Community Investment Dotnet Mvc/CommunityInvestment/CommunityInvestment/Controllers/HomeController.cs
@@ -22,27 +22,5 @@ namespace CommunityInvestment.Controllers
         {
             return View();
         }
-
-        public IActionResult LostPassword()
-        {
-            return View();
-        }
-
-        public IActionResult ResetPassword()
-        {
-            return View();
-        }
-
-        public IActionResult Register()
-        {
-            return View();
-        }
-
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
