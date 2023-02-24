64789119da9d2b6cbe9e713f4ca3b58399c987ed
charchil08 <er.charchilkajaliya@gmail.com>
2023-02-24 09:16:20 +0530

    volunteering mission page

--- Community Investment Dotnet Mvc/CommunityInvestment/CommunityInvestment/wwwroot/js/missionDetails.js
+++ Community Investment Dotnet Mvc/CommunityInvestment/CommunityInvestment/wwwroot/js/missionDetails.js
@@ -45,3 +45,36 @@ $(document).ready(function () {
         document.getElementById("demo").src = document.querySelector(".slide.active").src;
     }
 });
+
+//TODO - Recent volunteers js
+
+$(document).ready(function () {
+    const prev_btn = document.querySelector(".prev_volunteer>button");
+    const next_btn = document.querySelector(".next_volunteer>button");
+    const box = document.querySelector(".recent_volunteers_wrapper");
+    let current = 2;
+    let prev = 1;
+    let next = 2;
+    let totalVol = 27;
+    let totalVolInBox = 9;
+    let totalPage = totalVol / totalVolInBox;
+
+    prev_btn.addEventListener('click', () => {
+        if (current <= 1) return;
+        const vols = document.querySelectorAll('.recent_volunteers_wrapper > *');
+        /*debugger;*/
+        box.classList.add("d-none");
+        vols.forEach(vol => {
+            vol.style.overflow = 'hidden';
+        })
+        box.style.transform = `translateX(-100%)`;
+        debugger;
+        setTimeout(() => {
+            box.style.transform = `translateX(100%)`;
+            box.classList.remove('invisible');
+            box.style.transform = `translateX(0%)`;
+        }, 2000);
+
+        console.log(box)
+    })
+});
