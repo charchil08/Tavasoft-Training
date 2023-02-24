a8c9e48b98e898ff164625d56e63e9ae0227ff99
charchil08 <er.charchilkajaliya@gmail.com>
2023-02-24 14:59:00 +0530

    story listing page

---
+++ Community Investment Dotnet Mvc/CommunityInvestment/CommunityInvestment/Controllers/StoryController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CommunityInvestment.Controllers
+{
+    public class StoryController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public StoryController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
