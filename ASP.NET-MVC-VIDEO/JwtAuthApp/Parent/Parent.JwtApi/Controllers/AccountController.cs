397bb6105169210f5e938498f39b4cc2dce42dfc
charchil08 <er.charchilkajaliya@gmail.com>
2023-03-01 13:56:50 +0530

    custom auth filter - auth not working properly

---
+++ ASP.NET-MVC-VIDEO/JwtAuthApp/Parent/Parent.JwtApi/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcJwt.WebApiJwt;
+
+namespace Parent.JwtApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+       
+        [HttpGet]
+        public IActionResult ValidateLogin(string Username, string Password) 
+        {
+            if(Username == "admin" && Password == "admin")
+            {
+                return Ok(TokenManager.GenerateToken(Username));
+            }
+            return BadRequest("User not found");
+        }
+        
+
+        [HttpGet("validate")]
+        [CustomAuthenticationFilter]
+        public IActionResult GetEmp()
+        {
+            return Ok("Hi");
+        }
+    }
+}
---
+++ ASP.NET-MVC-VIDEO/JwtAuthApp/Parent/Parent.JwtApi/CustomAuthenticationFilter.cs
@@ -0,0 +1,78 @@
+﻿using System.Net;
+using System.Net.Http.Headers;
+using System.Web.Http;
+using System.Web.Http.Filters;
+
+namespace MvcJwt.WebApiJwt
+{
+    public class CustomAuthenticationFilter : System.Web.Http.AuthorizeAttribute, IAuthenticationFilter
+    {
+        public bool AllowMultiple => false;
+
+        public async Task AuthenticateAsync(HttpAuthenticationContext context, CancellationToken cancellationToken)
+        {
+            string authParameter = string.Empty;
+            HttpRequestMessage request = context.Request;
+
+            AuthenticationHeaderValue? authorization = request.Headers.Authorization;
+
+            if (authorization == null)
+            {
+                context.ErrorResult = new AuthenticationFailureResult("Missing authorization Header", request);
+                return;
+            }
+
+            if (authorization!=null && authorization.Scheme != "Bearer")
+            {
+                context.ErrorResult = new AuthenticationFailureResult("Invalid Authorization schema", request);
+                return ;
+            }
+
+            if(string.IsNullOrEmpty(authorization.Parameter))
+            {
+                context.ErrorResult = new AuthenticationFailureResult("Missing token", request);
+                return;
+            }
+
+            context.Principal = TokenManager.GetPrincipal(authorization.Parameter);
+        }
+
+        public async Task ChallengeAsync(HttpAuthenticationChallengeContext context, CancellationToken cancellationToken)
+        {
+            var result = await context.Result.ExecuteAsync(cancellationToken);
+            if(result.StatusCode == HttpStatusCode.Unauthorized)
+            {
+                result.Headers.WwwAuthenticate.Add(new AuthenticationHeaderValue("Basic","realm=localhost"));
+            }
+            //context.Result = 
+        }
+    }
+
+
+    //Make a new class
+    public class AuthenticationFailureResult : IHttpActionResult
+    {
+        public string ReasonPhrase;
+        public HttpRequestMessage Request { get; set; }
+
+        public AuthenticationFailureResult(string reasonPhrase, HttpRequestMessage request)
+        {
+            this.ReasonPhrase = reasonPhrase;
+            this.Request = request;
+        }
+
+        public HttpResponseMessage Execute()
+        {
+            HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.Unauthorized);
+
+            response.RequestMessage = Request;
+            response.ReasonPhrase = ReasonPhrase;
+            return response;
+        }
+
+        public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            return Task.FromResult(Execute());
+        }
+    }
+}
