2b00f16606b74fa504f661e4684c0ae017f9b297
charchil08 <er.charchilkajaliya@gmail.com>
2023-03-01 10:04:35 +0530

    auth - dummy project

---
+++ ASP.NET-MVC-VIDEO/JwtAuthApp/MvcJwt/MvcJwt.WebApiJwt/TokenManager.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace MvcJwt.WebApiJwt
+{
+    public class TokenManager
+    {
+        //use in appconfig.json
+        private static readonly string secret = "officejwttoken";
+
+        public static string GenerateToken(string UserName)
+        {
+            byte[] Key = Convert.FromBase64String(secret);
+            SymmetricSecurityKey securityKey = new SymmetricSecurityKey(Key);
+            SecurityTokenDescriptor securityTokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[] { new Claim(ClaimTypes.Name, UserName) }),
+                Expires = DateTime.UtcNow.AddMinutes(160),
+                SigningCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            JwtSecurityTokenHandler jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+            JwtSecurityToken token = jwtSecurityTokenHandler.CreateJwtSecurityToken(securityTokenDescriptor);
+
+            return jwtSecurityTokenHandler.WriteToken(token);
+        }
+    }
+}
