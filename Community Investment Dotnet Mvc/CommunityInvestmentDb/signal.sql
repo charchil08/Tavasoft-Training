4f6229d22c3726b41ec2bb5a461eaaeb49c4708d
charchil08 <er.charchilkajaliya@gmail.com>
2023-03-01 09:30:27 +0530

    signal.sql modified

--- Community Investment Dotnet Mvc/CommunityInvestmentDb/signal.sql
+++ Community Investment Dotnet Mvc/CommunityInvestmentDb/signal.sql
@@ -1,6 +1,14 @@
+
+use CommunityInvestment
+go
+
 select * from dbo.[user]
 go
 
-truncate table dbo.password_reset
 
+truncate table dbo.password_reset
+go
+
+select * from dbo.password_reset
+go
 -- hlxjrtuyfztfxmii
