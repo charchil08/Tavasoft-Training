367f5cc815c11a6007a8352806db8b4b63246738
charchil08 <er.charchilkajaliya@gmail.com>
2023-03-06 17:59:12 +0530

    New records entered

--- Community Investment Dotnet Mvc/CommunityInvestmentDb/entry.sql
+++ Community Investment Dotnet Mvc/CommunityInvestmentDb/entry.sql
@@ -109,4 +109,33 @@
 	  ('Planning', 1);
 
 
-select * from city;
+
+
+INSERT INTO [dbo].[mission] ([mission_theme_id], [city_id], [country_id], [title], [short_description], [description], [start_date], [end_date], [mission_type_id], [status], [organization_name], [organization_detail], [availability_id])
+VALUES
+(1, 2, 1, 'Teaching English in rural areas', 'Volunteer to teach English in rural areas', 'We are looking for volunteers to teach English to students in remote rural areas. No previous teaching experience required. Accommodation and meals will be provided.', '2023-05-01', '2023-06-30', 1, 0, 'ABC Organization', 'Helping students to achieve their dreams', 2),
+
+(2, 4, 1, 'Building homes for the homeless', 'Help us build homes for the homeless', 'Join us in building affordable homes for the homeless. We need volunteers who are willing to get their hands dirty and help with construction work. Accommodation and meals will be provided.', '2023-06-01', '2023-08-31', 1, 1, 'XYZ Foundation', 'Providing shelter for the homeless', 1),
+
+(3, 6, 1, 'Medical mission in a developing country', 'Join us in providing medical care in a developing country', 'We are looking for medical professionals to join us in providing medical care to underserved communities in a developing country. Accommodation and meals will be provided.', '2023-09-01', '2023-09-15', 1, 0, 'Medical Missions International', 'Improving global health', 3),
+
+(4, 8, 1, 'Environmental conservation project', 'Help us protect the environment', 'Join us in our efforts to protect the environment by participating in various conservation activities such as tree planting, wildlife monitoring, and environmental education. Accommodation and meals will be provided.', '2023-08-01', '2023-08-31', 1, 1, 'Green Earth Organization', 'Safeguarding the planet for future generations', 2),
+
+(5, 10, 1, 'Disaster relief mission', 'Assist in disaster relief efforts', 'We are looking for volunteers to help with disaster relief efforts in areas affected by natural disasters. Tasks may include distributing relief supplies, providing medical care, and coordinating with local authorities. Accommodation and meals will be provided.', '2023-07-01', '2023-07-15', 1, 0, 'Red Cross', 'Responding to emergencies worldwide', 1),
+
+(6, 11, 1, 'Orphanage volunteering', 'Volunteer at an orphanage', 'Join us in volunteering at an orphanage and help provide care and support to orphaned children. Tasks may include teaching, playing with the children, and assisting with daily activities. Accommodation and meals will be provided.', '2023-10-01', '2023-11-30', 2, 1, 'Hope Foundation', 'Supporting orphaned children', 2);
+
+go
+
+
+INSERT INTO [dbo].[mission_document] ([mission_id], [document_name], [doucment_type], [document_path])
+VALUES
+(4, 'Mission Image 1', 'image', 'D://CHARCHIL//Tavasoft-Training//Community Investment Dotnet Mvc//CommunityInvestment//CommunityInvestment//wwwroot//assets//CSR-initiative-stands-for-Coffee--and-Farmer-Equity.png'),
+(2, 'Mission Image 2', 'image', 'D://CHARCHIL//Tavasoft-Training//Community Investment Dotnet Mvc//CommunityInvestment//CommunityInvestment//wwwroot//assets//Education-Supplies-for-Every--Pair-of-Shoes-Sold.png'),
+(3, 'Mission Image 3', 'image', 'D://CHARCHIL//Tavasoft-Training//Community Investment Dotnet Mvc//CommunityInvestment//CommunityInvestment//wwwroot//assets//Grow-Trees-On-the-path-to-environment-sustainability.png'),
+(7, 'Mission Image 4', 'image', 'https://example.com/mission-image-4.jpg'),
+(5, 'Mission Image 5', 'image', 'https://example.com/mission-image-5.jpg'),
+(6, 'Mission Image 6', 'image', 'https://example.com/mission-image-6.jpg')
+go
+
+
--- Community Investment Dotnet Mvc/CommunityInvestmentDb/signal.sql
+++ Community Investment Dotnet Mvc/CommunityInvestmentDb/signal.sql
@@ -12,3 +12,9 @@ go
 select * from dbo.password_reset
 go
 -- hlxjrtuyfztfxmii
+
+delete from dbo.[user] 
+where user_id in (7)
+go
+
+
